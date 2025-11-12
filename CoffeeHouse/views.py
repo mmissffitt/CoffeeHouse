45730ca8826f94d7ea5be88859d9e24f7ceb4ef5
Студент группы IS <is@sielom.local>
2025-11-12 09:45:26 +0500

    реализован поиск

--- CoffeeHouse/views.py
+++ CoffeeHouse/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, HttpResponse, get_object_or_404
-from .models import Products, CategoriesOfProducts 
+from .models import Products, CategoriesOfProducts
 
 def root(request):
     return render(request, 'main-catalog.html')
@@ -23,11 +23,21 @@ def catalog(request):
         except CategoriesOfProducts.DoesNotExist:
             # если категория не найдена, ничего не фильтруем 
             pass
-            
+
+    # --- код для поиска аааа ---
+    # получаем параметр 'search' из GET запроса
+    search_query = request.GET.get('search')
+    
+    # если параметр 'search' существует и не пустой
+    if search_query:
+        # фильтруем продукты по названию, игнорируя регистр
+        products = products.filter(name__icontains=search_query)
+
     return render(request, 'catalog.html', {
         'products': products,
         'categories': all_categories,
-        'selected_category_id': int(selected_category_id) if selected_category_id else None, 
+        'selected_category_id': int(selected_category_id) if selected_category_id else None,
+        'search_query': search_query,
     })
 
 def product_detail(request, pk):
